97a24a578d059fa6bb7fd2d5653ce1e0a0e8b327
mironenko_ev@mail.ru <mironenko_ev@mail.ru>
2022-04-11 16:43:29 +0500

    1

--- MainWindow/main.cpp
+++ MainWindow/main.cpp
@@ -1,4 +1,11 @@
-﻿#include<Windows.h>
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<winapifamily.h>
+#include<Windows.h>
+#include<stdio.h>
+#include"resource.h"
+
+#define IDC_MY_STATIC 104
+
 CONST CHAR g_szClassName[] = "My Window Class";//global sz-string ziro
 LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
@@ -15,10 +22,11 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCMDLine, in
 	wc.lpfnWndProc = WndProc;//задаем процедуру окна для данного класса
 	wc.style = 0;//стиль окна.дополнительные свойства
 	//набор возможных стилей зависит от класса окна
-
-	wc.hIcon = LoadIcon(hInstance, MAKEINTRESOURCEA(IDI_APPLICATION));//в понели задач
-	wc.hIconSm = LoadIcon(hInstance, MAKEINTRESOURCEA(IDI_APPLICATION));//отражается в заголовке
-	wc.hCursor = LoadCursor(hInstance, MAKEINTRESOURCE(IDC_ARROW));
+	wc.hIcon = (HICON)LoadImage(hInstance, "phone.ico", IMAGE_ICON, LR_DEFAULTSIZE, LR_DEFAULTSIZE, LR_LOADFROMFILE);
+	wc.hIconSm = (HICON)LoadImage(hInstance, "sleep.ico", IMAGE_ICON, LR_DEFAULTSIZE, LR_DEFAULTSIZE, LR_LOADFROMFILE);
+	//wc.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_ICON1));//в понели задач
+	//wc.hIconSm = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_ICON2));//отражается в заголовке
+	wc.hCursor = LoadCursor(hInstance, MAKEINTRESOURCE(IDC_CURSOR1));
 	wc.hbrBackground = (HBRUSH)COLOR_WINDOW;
 	wc.hInstance = hInstance;
 	wc.lpszMenuName = NULL;
@@ -32,14 +40,20 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCMDLine, in
 		return 0;
 	}
 	//2)Создание окна
+	int screen_width = GetSystemMetrics(SM_CXSCREEN);//получает некоторые параметры системы.см-систем метрик размер экрана по х
+	int screen_height = GetSystemMetrics(SM_CYSCREEN);//по y
+	int window_start_x = screen_width / 8;
+	int window_start_y = screen_height / 8;
+	int window_width = screen_width - screen_width / 4;
+	int window_height = screen_height - screen_height / 4;
 	HWND hwnd = CreateWindowEx
 	(
 		NULL,
 		g_szClassName,//имя класса
 		"Main Window",//заголовок
 		WS_OVERLAPPEDWINDOW,//стиль главного окна приложения
-		CW_USEDEFAULT, CW_USEDEFAULT,//положение окна на экране
-		CW_USEDEFAULT, CW_USEDEFAULT,//размер окна по умолчанию
+		window_start_x, window_start_y,//положение окна на экране
+		window_width, window_height,//размер окна по умолчанию
 		NULL,//родительское окно , оно отсутствует
 		NULL,//меню или id дочернего окна //id меню окна,если оно главное.или ресурса,если дочернее
 		hInstance,
@@ -68,6 +82,37 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	{
 	case WM_CREATE: //создаются элемнты окна,кнопки,меню,значки аналог WM_INITDIALOG
 		//это сообщение отрабатывает один раз
+		CreateWindowEx
+		(
+			NULL,
+			"Static",
+			"Это мой статический текст, в котором будет отображаться размер окна и его положение на экране",
+			WS_CHILDWINDOW|WS_VISIBLE,
+			10,10,
+			10000,50,
+			hwnd,
+			(HMENU)IDC_MY_STATIC,
+			GetModuleHandle(NULL),
+			NULL
+
+		);
+	
+		break;
+	case WM_MOVE:
+
+	case WM_SIZE:
+	{
+		CONST INT SIZE = 256;
+		CHAR buffer[SIZE] = {};
+		RECT rect;//объявляем прямоугольник
+		GetWindowRect(hwnd, &rect);
+		int window_width = rect.right - rect.left;
+		int window_height = rect.bottom - rect.top;
+		sprintf(buffer, "Size:%dx%d, Position:%dx%d" , window_width, window_height,rect.left,rect.top);
+		SendMessage(hwnd, WM_SETTEXT, 0, (LPARAM)buffer);
+		HWND hStatic = GetDlgItem(hwnd, IDC_MY_STATIC);
+		SendMessage(hStatic, WM_SETTEXT, 0, (LPARAM)buffer);
+	}
 		break;
 	case WM_COMMAND:break;
 	case WM_CLOSE:
